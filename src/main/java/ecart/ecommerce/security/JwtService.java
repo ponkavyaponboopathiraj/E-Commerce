c1857e670d40a94a5da8b795bec36588b4d074f9
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-07-20 21:25:50 +0530

    Added the jwt service file

--- src/main/java/ecart/ecommerce/security/JwtService.java
+++ src/main/java/ecart/ecommerce/security/JwtService.java
@@ -1,4 +1,103 @@
-package ecart.ecommerce.security;
+package ecart.ecommerce.service;
+
+import ecart.ecommerce.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
-    
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    /**
+     * Generate JWT Token
+     */
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    /**
+     * Secret Key
+     */
+    private SecretKey getSignInKey() {
+
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    /**
+     * Extract Email
+     */
+    public String extractUsername(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    /**
+     * Extract Expiration
+     */
+    public Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    /**
+     * Generic Claim Extractor
+     */
+    public <T> T extractClaim(String token,
+                              Function<Claims, T> claimsResolver) {
+
+        Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    /**
+     * Read all Claims
+     */
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    /**
+     * Check Token Expired
+     */
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token)
+                .before(new Date());
+    }
+
+    /**
+     * Validate Token
+     */
+    public boolean isTokenValid(String token, User user) {
+
+        String username = extractUsername(token);
+
+        return username.equals(user.getEmail())
+                && !isTokenExpired(token);
+    }
 }
