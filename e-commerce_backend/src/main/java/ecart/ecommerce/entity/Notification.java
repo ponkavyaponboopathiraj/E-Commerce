171779f3a9c29c4027e13702fa83aad60148d00f
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-08-14 20:48:30 +0530

    Notification added

---
+++ e-commerce_backend/src/main/java/ecart/ecommerce/entity/Notification.java
@@ -0,0 +1,84 @@
+package ecart.ecommerce.entity;
+import ecart.ecommerce.enums.NotificationType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+    private String sellerId;
+    private String productId;
+    private String productName;
+    private NotificationType type;
+    private String message;
+    private boolean read = false;
+    private LocalDateTime createdAt;
+
+    public Notification() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
