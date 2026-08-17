739c5043f9f3bebb8ac263355c82a9fd5d803873
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-08-17 22:33:36 +0530

    Adding service fiile

---
+++ e-commerce_backend/src/main/java/ecart/ecommerce/service/OrderService.java
@@ -0,0 +1,17 @@
+package ecart.ecommerce.service;
+
+import ecart.ecommerce.entity.Order;
+import ecart.ecommerce.enums.OrderStatus;
+
+import java.util.List;
+
+public interface OrderService {
+
+    Order placeOrder(Order order);
+    Order getOrderById(String orderId);
+    List<Order> getAllOrders();
+    List<Order> getOrdersByCustomer(String customerId);
+    List<Order> getOrdersBySeller(String sellerId);
+    Order updateOrderStatus(  String orderId,OrderStatus status);
+    Order cancelOrder(String orderId);
+}
